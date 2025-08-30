7b1a91e85eccb84b308b9c355b956330f009c6c6
supun <supun.sarachith@gmail.com>
2025-08-29 20:06:22 -0400

    base model classes addes

---
+++ SmartShop.API/Models/Customer.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace SmartShop.API.Models
+{ 
+    public class Customer
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+    }
+}
